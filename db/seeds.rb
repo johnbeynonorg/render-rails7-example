391d07a04fa7a418477c41fe484c2d9ad3f5b6bc
John Beynon <john@render.com>
2022-03-22 12:02:48 +0000

    Master Detail demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+1.upto(10000) do |i|
+	Post.create(title: "This is the title #{i}")
+end
