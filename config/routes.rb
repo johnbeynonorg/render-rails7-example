a02421ed911972c21eb5fa93366f575c8a3ee3b2
John Beynon <john@render.com>
2022-03-22 12:28:01 +0000

    Tweak

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/health', to: 'health#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
