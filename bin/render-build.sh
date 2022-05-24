18b5afe831379d34933fdae50d53e492a89ad92b
John Beynon <john@render.com>
2022-05-24 08:55:27 +0100

    FreeDTS

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -20,3 +20,26 @@ then
 else
   echo "Not a web process so nothing to do here."  
 fi
+
+mkdir -p ~/tmp
+mkdir -p ~/local/src/freedts
+
+wget -P ~/tmp ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.3.10.tar.gz
+tar -xvzf freetds-1.3.10.tar.gz
+cd ~/tmp
+./configure --help
+./configure
+make
+
+
+#dpkg -x ~/tmp/google-chrome-stable_current_amd64.deb ~/local/src/chrome/
+#export PATH="${PATH}:${HOME}/local/src/chrome/opt/google/chrome/"
+#rm ~/tmp/google-chrome-stable_current_amd64.deb
+#rmdir ~/tmp
+
+
+# $ ./configure --help
+# $ ./configure
+# $ make
+# $ make install
+
