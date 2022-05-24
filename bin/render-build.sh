abda2e36db31346d96fe75a0d9d3bef046c46999
John Beynon <john@render.com>
2022-05-24 11:31:27 +0000

    TDS not DTS

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,26 +3,26 @@
 set -o errexit
 
 # Install FreeDTS and build if it's not in the cache
-if [[ ! -d /opt/render/project/nodes/freedts ]]; then
-  echo "Downloading and compiling FreeDTS"
+if [[ ! -d /opt/render/project/nodes/freetds ]]; then
+  echo "...Downloading and compiling FreeTDS"
   mkdir -p ~/tmp
-  mkdir -p ~/local/bin/freedts
+  mkdir -p ~/local/bin/freetds
 
   wget -P ~/tmp ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.3.10.tar.gz
   cd ~/tmp
   tar -xzf freetds-1.3.10.tar.gz
   cd freetds-1.3.10
   autoconf
-  ./configure --prefix=/opt/render/project/nodes/freedts
+  ./configure --prefix=/opt/render/project/nodes/freetds
   make
   make install
   cd $HOME/project/src # Make sure we return to where we were
 else
-  echo "Using FreeDTS from build cache"
+  echo "...Using FreeTDS from build cache"
 fi
 
 # Set a bundler config option with the path to the freedts directory
-bundle config build.tiny_tds --with-freetds-dir=/opt/render/project/nodes/freedts/
+bundle config build.tiny_tds --with-freetds-dir=/opt/render/project/nodes/freetds/
 
 # We always want these run
 bundle config set --local without 'development test'
