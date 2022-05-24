e564dc43c3cd0ac4849f1a617e346382963107a4
John Beynon <john@render.com>
2022-05-24 16:31:34 +0000

    Change to using XDG_CACHE_HOME

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,18 +2,17 @@
 # exit on error
 set -o errexit
 
-# Install FreeDTS and build if it's not in the cache
-if [[ ! -d /opt/render/project/nodes/freetds ]]; then
+# Download FreeTDS and compile if it's not in the cache
+if [[ ! -d $XDG_CACHE_HOME/freetds ]]; then
   echo "...Downloading and compiling FreeTDS"
   mkdir -p ~/tmp
-  mkdir -p ~/local/bin/freetds
 
   wget -P ~/tmp ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.3.10.tar.gz
   cd ~/tmp
   tar -xzf freetds-1.3.10.tar.gz
   cd freetds-1.3.10
   autoconf
-  ./configure --prefix=/opt/render/project/nodes/freetds
+  ./configure --prefix=$XDG_CACHE_HOME/freetds
   make
   make install
   cd $HOME/project/src # Make sure we return to where we were
@@ -21,8 +20,8 @@ else
   echo "...Using FreeTDS from build cache"
 fi
 
-# Set a bundler config option with the path to the freedts directory
-bundle config build.tiny_tds --with-freetds-dir=/opt/render/project/nodes/freetds/
+# Set a bundler config option with the path to the freeTDS directory
+bundle config build.tiny_tds --with-freetds-dir=$XDG_CACHE_HOME/freetds/
 
 # We always want these run
 bundle config set --local without 'development test'
